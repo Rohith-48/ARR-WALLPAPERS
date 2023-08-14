43209c078f0a5d4b21b96cee956574f7ff0cff77
Rohit <rohithsanthosh2024@mca.ajce.in>
2023-08-14 12:49:24 +0530

    vercel_deployed

--- arrwallpapers/arrwallpapers/settings.py
+++ arrwallpapers/arrwallpapers/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-n2kk#r)-fk*dzjhk)i((epyzzalj84j3qx^o80%x8(j@4nb-jm
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
