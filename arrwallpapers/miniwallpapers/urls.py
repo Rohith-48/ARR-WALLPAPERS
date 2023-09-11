087a1a6dfd92a76fc5dece09609d26968e4c8096
Rohit <rohithsanthosh2024@mca.ajce.in>
2023-09-11 16:27:05 +0530

    forgot_pass_done

--- arrwallpapers/miniwallpapers/urls.py
+++ arrwallpapers/miniwallpapers/urls.py
@@ -2,6 +2,7 @@ from django.conf import settings
 from django.urls import path
 from . import views
 from django.conf.urls.static import static
+from django.contrib.auth import views as auth_views
 
 
 
@@ -19,8 +20,15 @@ urlpatterns=[
     path('upload_wallpaper/', views.upload_wallpaper, name='upload_wallpaper'),
     path('view_delete_wallpaper/', views.view_delete_wallpaper, name='view_delete_wallpaper'),
     path('update_wallpaper/', views.update_wallpaper, name='update_wallpaper'),
-        
-        
+
+
+     path('password_reset/',auth_views.PasswordResetView.as_view(),name='password_reset'),
+     path('password_reset/done/',auth_views.PasswordResetDoneView.as_view(),name='password_reset_done'),
+     path('reset/<uidb64>/<token>/',auth_views.PasswordResetConfirmView.as_view(),name='password_reset_confirm'),
+     path('reset/done/',auth_views.PasswordResetCompleteView.as_view(),name='password_reset_complete'),
+
+
+
     path('user_upload/', views.user_upload, name='user_upload'),
     path('user_edit/', views.user_edit_wallpaper, name='user_edit'),
     path('view_delete_userwallpaper/', views.view_delete_userwallpaper, name='view_delete_userwallpaper'),  
@@ -29,7 +37,12 @@ urlpatterns=[
     path('wallpaper/<int:wallpaper_id>/', views.wallpaper_details, name='wallpaper_details'),
     # path('search/', views.search_wallpapers, name='search_wallpapers'),
     path('liked_wallpapers/', views.liked_wallpapers, name='liked_wallpapers'),
-    path('subscribe_page/', views.subscribe_page, name='subscribe_page'),]
+    path('subscribe_page/', views.subscribe_page, name='subscribe_page'),
+    
+    
+    
+    
+    ]
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
