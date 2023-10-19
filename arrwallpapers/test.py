4cbe397d8d0df29f19a7ac77657b7c83aa12b3d6
Rohith <rohithsanthosh2024@mca.ajce.in>
2023-10-19 11:26:33 +0530

    figma changes done

--- arrwallpapers/test.py
+++ arrwallpapers/test.py
@@ -9,7 +9,6 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 class Hosttest(TestCase):
-    
     def setUp(self):
         self.driver = webdriver.Chrome()
         self.driver.implicitly_wait(10)
